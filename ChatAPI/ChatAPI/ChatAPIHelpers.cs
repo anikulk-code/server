3194f988edf44829273113a7e4fb0d784759a534
Aniruddha Kulkarni <aniruddhattu@gmail.com>
2025-04-06 10:18:14 -0700

    Add project files.

---
+++ ChatAPI/ChatAPI/ChatAPIHelpers.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.AI.Inference;
+
+namespace ChatAPI
+{
+    internal static class ChatAPIHelpers
+    {
+
+        public static async Task<string> callAzureService(string chatMessage)
+        {
+            var endpoint = new Uri(Environment.GetEnvironmentVariable("API_URL"));
+            var credential = new AzureKeyCredential(Environment.GetEnvironmentVariable("API_KEY"));
+            var model = "gpt-4o-mini";
+
+            var client = new ChatCompletionsClient(
+                endpoint,
+                credential);
+
+            var requestOptions = new ChatCompletionsOptions()
+            {
+                Messages =
+                {
+                    new ChatRequestUserMessage(chatMessage)
+                },
+                MaxTokens = 4096,
+                Temperature = 1.0f,
+                //top_p = 1.0f,
+                Model = model
+            };
+
+            Response<ChatCompletions> response = await client.CompleteAsync(requestOptions);
+            return response.Value.Content;
+        }
+    }
+}
